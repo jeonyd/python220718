9b291797b2cbc92e63f78142ed394fcac8c9cd27
jeonyd <doccl123@naver.com>
2022-07-19 09:54:16 +0900

    함수 연습6

--- function4.py
+++ function4.py
@@ -19,3 +19,16 @@ def union(*ar):
 
 #호출
 print(union("HAM","EGG"))
+
+#정의되지 않은 인자 (앞에 두개 필수, 딕셔너리 옵션)
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL
+
+#호출
+
+print(userURIBuilder("credu.com","80",id="kim",passwd="1234"))
+print(userURIBuilder("credu.com","80",id="kim",passwd="1234",
+    name = "mike"))
