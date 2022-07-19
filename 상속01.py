c756273aee48c091906cabe9773b06ca3ab080e6
jeonyd <doccl123@naver.com>
2022-07-19 15:36:39 +0900

    클래스 연습 완료

--- 상속01.py
+++ 상속01.py
@@ -8,15 +8,25 @@ class Person:
 
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        # self.name = name
+        # self.phoneNumber = phoneNumber
+        #부모의 생성자 호출 (위 두 줄과 같은 효과)
+        Person.__init__(self,name,phoneNumber)
+
         self.subject = subject
         self.studentID = studentID
 
+    #상속받아서 메서드 재정의(override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
+
+# 인스턴스 생성
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 print(p.__dict__)
 print(s.__dict__)
 
-
+p.printInfo()
+s.printInfo()
