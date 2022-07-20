d291da39a8f30b3bcffca0e45bacfd59b9b82bf3
jeonyd <doccl123@naver.com>
2022-07-20 14:41:40 +0900

    re모듈

--- DemoRE.py
+++ DemoRE.py
@@ -1,9 +1,12 @@
 import re
 
-result = re.search("[0-9]*th","35th")
+result = re.search("[0-9]*th","  35th")
 print(result)
 print(result.group())
 
-result = re.match("[0-9]*th","35th")
+result = re.match("[0-9]*th","35th")  #완벽하게 일치해야 함
 print(result)
 print(result.group())
+
+print(bool(re.search("apple","this is apple")))
+print(bool(re.match("apple","this is apple")))
