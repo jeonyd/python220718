13164703212412e95199275eda682163b3584e28
jeonyd <doccl123@naver.com>
2022-07-20 16:46:19 +0900

    db

--- db2.py
+++ db2.py
@@ -1,8 +1,8 @@
 
 import sqlite3
 
-#연결객체 리턴 (일단은 임시로 메모리에서 작업)
-con = sqlite3.connect(":memory:")
+#연결객체 리턴 (데이터베이스 파일에 저장)
+con = sqlite3.connect("test.db")
 #커서 객체(SQL구문을 실행하는 객체)
 cur = con.cursor()
 #테이블 생성
