e28cb3163032629a192ef287fc600f0a84777624
jeonyd <doccl123@naver.com>
2022-07-21 15:26:48 +0900

    시그널 처리

---
+++ DemoForm2.py
@@ -0,0 +1,34 @@
+# DemoForm.py
+# DemoForm2.ui(화면단) + DemoForm2.py(로직단)
+
+import sys
+from PyQt5. QtWidgets import *
+from PyQt5 import uic
+
+# 폼디자인을 로딩
+form_class = uic.loadUiType("DemoForm2.ui")[0]
+
+# 클래스 정의(QMainWindow)
+class DemoForm(QMainWindow, form_class):
+    #초기화 매서드
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+    #슬롯 메서드
+    def firstClick(self):
+        self.label.setText("첫번째 클릭")
+    
+    def secondClick(self):
+        self.label.setText("두번째 클릭")
+        
+    def thirdClick(self):
+        self.label.setText("세번째 클릭")
+
+
+# 진입점을 체크
+if __name__== "__main__":
+    app = QApplication(sys.argv)
+    demoWindow = DemoForm()
+    demoWindow.show()
+    app.exec_()
