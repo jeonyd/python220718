0694d93d57c4b89ab8a2e0cf1a72c82f5c997215
jeonyd <doccl123@naver.com>
2022-07-21 15:33:21 +0900

    gui로웹툰수집

--- DemoForm2.py
+++ DemoForm2.py
@@ -5,6 +5,12 @@ import sys
 from PyQt5. QtWidgets import *
 from PyQt5 import uic
 
+#웹서버와 통신할 경우
+import urllib.request
+#크롤링
+from bs4 import BeautifulSoup
+
+
 # 폼디자인을 로딩
 form_class = uic.loadUiType("DemoForm2.ui")[0]
 
@@ -17,7 +23,33 @@ class DemoForm(QMainWindow, form_class):
         
     #슬롯 메서드
     def firstClick(self):
-        self.label.setText("첫번째 클릭")
+
+        #파일로 저장
+        f = open("webtoon.txt","wt",encoding="utf-8")
+
+        #페이지 처리를 위해 range함수 사용
+        for i in range(1,6):
+            url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+            print(url)
+
+            #네이버에서 실행한 페이지결과를 문자열로 받기
+            data = urllib.request.urlopen(url)
+            #검색이 용이한 객체
+            soup = BeautifulSoup(data,"html.parser")
+            #특정한 태그를 검색
+            cartoons = soup.find_all("td", class_="title")
+
+
+            for item in cartoons:
+                item2 = item.find("a")
+                title = item2.text.strip()
+                print(title)
+                f.write(title + "\n" )
+
+        f.close()
+        self.label.setText("웹툰 크롤링 종료")
+
+
     
     def secondClick(self):
         self.label.setText("두번째 클릭")
